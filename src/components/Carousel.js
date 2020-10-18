5432320746ddec06e8c395e9fcad9a94a5d28d75
Jessica Ellis <jess.patnode1@gmail.com>
2020-10-17 23:16:03 -0600

    feat(carousel component): add carousel component and styling
    Contributes to, but doesn't close #92. Provides a generic carousel component that dynamically
    populates content as they slide on screen.

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,27 +1,98 @@
-import React, { useMemo, useState } from 'react';
+import React, {
+  useMemo, useState, useEffect, useRef,
+} from 'react';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
 import Slide from '@material-ui/core/Slide';
 
-const Carousel = ({ children }) => {
+const Carousel = ({ children, showButtons }) => {
+  const [isPrimarySlide, setIsPrimarySlide] = useState(true);
+  const [isLeftward, setIsLeftward] = useState(true);
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [primaryContent, setPrimaryContent] = useState(children[0]);
+  const [secondaryContent, setSecondaryContent] = useState(<></>);
 
-  const slides = useMemo(() => {
-    return (<div>
+  const prevSlideRef = useRef();
+  useEffect(() => {
+    prevSlideRef.current = currentSlide;
+  });
+  const prevSlide = prevSlideRef.current;
 
-    </div>);
-  }, [children, currentSlide]);
+  useEffect(() => {
+    if (isPrimarySlide) {
+      setSecondaryContent(children[currentSlide]);
+    } else {
+      setPrimaryContent(children[currentSlide]);
+    }
+    setIsLeftward(prevSlide < currentSlide);
+    setIsPrimarySlide(!isPrimarySlide);
+  }, [currentSlide]);
 
-  return (<div className="carousel-container">
-    {children.length > 1
-      ? (
-        <Button className="carousel-leftArrow">&lt;</Button> // TODO: Wireup. Styling
-        <Button className="carousel-rightArrow">&gt;</Button> // TODO: Wireup. Styling
-        {slides}
-        // TODO: dot navigation
-      ) : (
-        children[0]
-      )}
-  </div>);
+  const slides = useMemo(() => (
+    <div>
+      <Slide
+        className="carousel-slide"
+        direction={isLeftward === isPrimarySlide ? 'left' : 'right'}
+        in={isPrimarySlide}
+        unmountOnExit
+        mountOnEnter
+      >
+        <div>{primaryContent}</div>
+      </Slide>
+      <Slide
+        className="carousel-slide"
+        direction={isLeftward === isPrimarySlide ? 'right' : 'left'}
+        in={!isPrimarySlide}
+        unmountOnExit
+        mountOnEnter
+      >
+        <div>{secondaryContent}</div>
+      </Slide>
+    </div>
+  ), [children, isPrimarySlide]);
+
+  return (
+    <div className="carousel-container">
+      {children.length > 1
+        ? (
+          <div>
+            {slides}
+            {showButtons
+              ? (
+                <div className="carousel-dot-group">
+                  {children.map((_, index) => (
+                    <Button
+                      className="carousel-dot-button"
+                      key={index}
+                      onClick={() => setCurrentSlide(index)}
+                    >
+                      <div className={`carousel-dot-label ${currentSlide === index ? 'selected' : ''}`} />
+                    </Button>
+                  ))}
+                </div>
+              )
+              : <></>}
+          </div>
+        ) : (
+          children[0]
+        )}
+    </div>
+  );
+};
+
+Carousel.propTypes = {
+  children: (props, propName, componentName) => {
+    const prop = props[propName];
+    if (!Array.isArray(prop) || prop.length() < 1) {
+      return new Error(`\`${componentName}\` must have at least one child.`);
+    }
+    return null;
+  },
+  showButtons: PropTypes.bool,
+};
+
+Carousel.defaultProps = {
+  showButtons: true,
 };
 
 export default Carousel;
